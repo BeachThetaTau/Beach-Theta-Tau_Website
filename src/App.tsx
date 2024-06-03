762757b435af4cc0c8eeebc71940ceeccb5c6c58
MarkCarsonDev <markcarson.dev@gmail.com>
2024-06-03 14:25:50 -0700

    Incorporated routing from authentication sandbox

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import Professionalism from "./pages/Professionalism.tsx";
 import Service from "./pages/Service.tsx";
 import Apply from "./pages/Apply.tsx";
 
+import Layout from "./Layout";
+import Login from "./Login";
+import Signup from "./Signup";
+import Profile from "./Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 {
 
 /*
@@ -18,8 +24,24 @@ import Apply from "./pages/Apply.tsx";
 */
 }
 
-const App = () => (
-<Apply/>
-);
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path = "/" element = { <Layout></Layout> }>
+            <Route index element = { <Home></Home> }></Route>
+            <Route path = "/login" element = { <Login></Login> } ></Route>
+            <Route path = "/signup" element = { <Signup></Signup> } ></Route>
+            <Route path = "/profile" element = { <Profile></Profile> }></Route>
+            <Route path = "/about" element = { <About></About> }></Route>
+            <Route path = "/social" element = { <Social></Social> }></Route>
+            <Route path = "/professionalism" element = { <Professionalism></Professionalism> }></Route>
+            <Route path = "/service" element = { <Service></Service> }></Route>
+            <Route path = "/apply" element = { <Apply></Apply> }></Route>
+          </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
-export default App;
+export default App
