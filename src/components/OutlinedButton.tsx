56d892d7e37ac282ecae3f94add8489669f7be00
MarkCarsonDev <markcarson.dev@gmail.com>
2024-06-03 14:48:53 -0700

    Updated linking to OutlinedButton

--- src/components/OutlinedButton.tsx
+++ src/components/OutlinedButton.tsx
@@ -1,22 +1,28 @@
 // OutlinedButton.tsx
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './OutlinedButton.css';
 
 interface OutlinedButtonProps {
   text: string;
-  fontSize?: string; 
+  fontSize?: string;
+  to: string; // The destination URL
 }
 
-const OutlinedButton: React.FC<OutlinedButtonProps> = ({ text, fontSize }) => {
+
+const OutlinedButton: React.FC<OutlinedButtonProps> = ({ text, fontSize, to }) => {
   return (
-    <button
-      className="outlined-button"
-      style={{ fontSize: fontSize || '0.75rem' }} // Set the font size dynamically or use a default value
-    >
-      {text}
-    </button>
+    <Link to={to}>
+      <button
+        className="outlined-button"
+        style={{ fontSize: fontSize || '0.75rem' }}
+      >
+        {text}
+      </button>
+    </Link>
   );
 };
 
-export default OutlinedButton;
+export default OutlinedButton;;
