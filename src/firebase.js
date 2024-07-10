8bcf111eceba435d957decb2ee77f5085ec8677f
MarkCarsonDev <markcarson.dev@gmail.com>
2024-07-10 15:26:43 -0700

    Regress to old config

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,9 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-if (process.env.FIREBASE_API === undefined) {
-  console.error("Firebase API key is not set. Please set the FIREBASE_API environment variable.");
-  if (process.env.FIREBASE_API_ === undefined) {
-    console.error("Backup is not set either...");
-  }
-}
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API ? process.env.FIREBASE_API : process.env.FIREBASE_API_,
+  apiKey: process.env.FIREBASE_API,
   authDomain: "xiepsilonmanager.firebaseapp.com",
   projectId: "xiepsilonmanager",
   storageBucket: "xiepsilonmanager.appspot.com",
