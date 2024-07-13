4cd9338ab72aa6b2d6770fd6c297075d3e8c116e
dominicAlfonso <Dominic21205@gmail.com>
2024-07-12 22:34:28 -0700

    fix process.env

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': import.meta
+    'process.env': process.env
   }
   
 })
