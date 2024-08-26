98ca4da3a3432c8fd0dfbf1ab98da8b5baba2cb3
DalfoDev <72036049+DalfoDev@users.noreply.github.com>
2024-08-26 14:49:03 -0700

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+console.log(process.env.VITE_PUBLIC)
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  
+  apiKey: process.env.FIREBASE_API,
+  authDomain: "xiepsilonmanager.firebaseapp.com",
+  projectId: "xiepsilonmanager",
+  storageBucket: "xiepsilonmanager.appspot.com",
+  messagingSenderId: "738436091091",
+  appId: "1:738436091091:web:9af249bbbb14ba354bd826"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth }
