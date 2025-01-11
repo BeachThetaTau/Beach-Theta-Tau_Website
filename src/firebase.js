d1f59f5d4f5fd072f3d7b894fcace2c51aa36683
dominicAlfonso <Dominic21205@gmail.com>
2025-01-10 17:54:57 -0800

    added database

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-console.log(process.env.VITE_PUBLIC)
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,8 +13,9 @@ const firebaseConfig = {
   appId: process.env.FIREBASE_APP_ID
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app); // Switch to Firestore
 
-export { auth }
+export { auth, db };
