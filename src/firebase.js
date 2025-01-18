8f1a7df83f7a4c9a1105733ce54628b5fc6ad67b
dominicAlfonso <Dominic21205@gmail.com>
2025-01-17 19:21:59 -0800

    created new keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase } from 'firebase/database';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API,
-  authDomain: "xiepsilonmanager.firebaseapp.com",
-  projectId: "xiepsilonmanager",
-  storageBucket: "xiepsilonmanager.appspot.com",
-  messagingSenderId: "738436091091",
-  appId: process.env.FIREBASE_APP_ID
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app); 
+const db = getDatabase(app);
 
 export { auth, db };
