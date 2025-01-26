fda3eeeff4e23969e439e4c3ebbd98bfa3f84c08
dominicAlfonso <Dominic21205@gmail.com>
2025-01-25 23:38:45 -0800

    reconfigured App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,34 @@
 import React from "react";
-import Home from "./pages/Home.tsx";
-import About from "./pages/About.tsx";
-import Social from "./pages/Social.tsx";
-import Professionalism from "./pages/Professionalism.tsx";
-import Service from "./pages/Service.tsx";
-import Apply from "./pages/Apply.tsx";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Profile from "./pages/Profile";
-import UnderConstruction from "./pages/UnderConstruction.tsx";
-
-import Layout from "./components/Layout.jsx";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer.tsx";
-import HamburgerMenu from "./components/HamburgerMenu.tsx";
-
-{
-  /*
-<Home/>
-<About/>
-<Social/>
-<Professionalism/>
-<Service/>
-<Apply/>
-*/
-}
+import Layout from "./components/Layout"; // Ensure this is a .tsx file
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Social from "./pages/Social";
+import Professionalism from "./pages/Professionalism";
+import Service from "./pages/Service";
+import Apply from "./pages/Apply";
+import Login from "./pages/Login"; // Ensure this is a .tsx file
+import Signup from "./pages/Signup"; // Ensure this is a .tsx file
+import Profile from "./pages/Profile"; // Ensure this is a .tsx file
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path = "/" element = { <Layout></Layout> }>
-            <Route index element = { <Home></Home> }></Route>
-            <Route path = "/login" element = { <Login></Login> } ></Route>
-            <Route path = "/signup" element = { <Signup></Signup> } ></Route>
-            <Route path = "/profile" element = { <Profile></Profile> }></Route>
-            <Route path = "/about" element = { <About></About> }></Route>
-            <Route path = "/social" element = { <Social></Social> }></Route>
-            <Route path = "/professionalism" element = { <Professionalism></Professionalism> }></Route>
-            <Route path = "/service" element = { <Service></Service> }></Route>
-            <Route path = "/apply" element = { <Apply></Apply> }></Route>
-            <Route path = "/under-construction" element = { <UnderConstruction></UnderConstruction> }></Route>
-            <Route path = "/apply" element = { <Apply></Apply> }></Route>
-          
-          </Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/social" element={<Social />} />
+          <Route path="/professionalism" element={<Professionalism />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/apply" element={<Apply />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
