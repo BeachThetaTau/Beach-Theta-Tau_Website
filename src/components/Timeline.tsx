e080740446aaa2bd6b70e27330551e30dccfb255
dominicAlfonso <Dominic21205@gmail.com>
2025-01-30 11:11:36 -0800

    chore: update rush timeline to reflect new deadlines and milestones

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -7,9 +7,9 @@ const RushTimeline = () => {
     <div id="timeline-container">
       <div className="inner-container">
         <ul className="timeline">
-          <li className="event" data-date="9/9 - 7:00 PM">
+          <li className="event" data-date="2/3 - 7:00 PM">
             <p className="event-name">INFO NIGHT</p>
-            <p className="event-date">9/9 - 7:00 PM</p>
+            <p className="event-date">2/3 - 7:00 PM</p>
             <p className="event-location">Online</p>
             <p className="event-description">
               Discover the gateway to exploring the opportunities and benefits
@@ -18,9 +18,9 @@ const RushTimeline = () => {
             </p>
           </li>
 
-          <li className="event" data-date="9/10 - 7:00 PM">
+          <li className="event" data-date="2/4 - 7:00 PM">
             <p className="event-name">ENGINEERING NIGHT</p>
-            <p className="event-date">9/10 - 7:00 PM</p>
+            <p className="event-date">2/4 - 7:00 PM</p>
             <p className="event-location">Health & Human Services Building</p>
             <p className="event-description">
               Connect with fellow rushees and collaborate alongside our
@@ -28,9 +28,9 @@ const RushTimeline = () => {
             </p>
           </li>
 
-          <li className="event" data-date="9/11 - 7:00 PM">
+          <li className="event" data-date="2/5 - 7:00 PM">
             <p className="event-name">PROFFESSIONAL NIGHT</p>
-            <p className="event-date">9/11 - 7:00 PM</p>
+            <p className="event-date">2/5 - 7:00 PM</p>
             <p className="event-location">Health & Human Services Building</p>
             <p className="event-description">
               Dress in your professional attire for a series of friendly speed
@@ -41,9 +41,9 @@ const RushTimeline = () => {
             </p>
           </li>
 
-          <li className="event" data-date="9/12 - 7:00 PM">
+          <li className="event" data-date="2/6 - 7:00 PM">
             <p className="event-name">GAME NIGHT</p>
-            <p className="event-date">9/12 - 7:00 PM</p>
+            <p className="event-date">2/6 - 7:00 PM</p>
             <p className="event-location">Health & Human Services Building</p>
             <p className="event-description">
               Dress in your{" "}
@@ -57,9 +57,9 @@ const RushTimeline = () => {
             </p>
           </li>
 
-          <li className="event" data-date="9/13 - 7:00 PM">
+          <li className="event" data-date="2/7 - 7:00 PM">
             <p className="event-name">SOCIAL NIGHT</p>
-            <p className="event-date">9/13 - 7:00 PM</p>
+            <p className="event-date">2/7 - 7:00 PM</p>
             <p className="event-location">TBD</p>
             <p className="event-description">
               Indulge in an afternoon of socializing with the members of Theta
